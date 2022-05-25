35cc0445a5a3f6f3b3cc33ecf5d35679877b0c18
dengjiawen <dengjiawen@bilibili.com>
2022-05-25 16:29:47 +0800

    print 'hello world'

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%s\n", "callmainfunction")
-	fmt.Println("Hello World")
 	fmt.Printf("%s\n", "call main function")
+	fmt.Println("Hello World")
 }
